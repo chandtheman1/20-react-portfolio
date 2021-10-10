ebaaf348b20c46476d732edc2785b1e9b554f9f8
Chandler Liang <chandler.liang123@gmail.com>
2021-10-10 14:14:33 +1100

    created the footer

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div>
+            <li>
+                <a
+                    href="https://github.com/chandtheman1"
+                >
+                    Github
+                </a>
+            </li>
+            <li>
+                <a
+                    href="https://www.linkedin.com/in/chandler-liang-1204b7133/"
+                >
+                    Linkedin
+                </a>
+            </li>
+        </div>
+    );
+};
+
+export default Footer;
