d452dd0225971866517188ee36484031af3f3d18
Chandler Liang <chandler.liang123@gmail.com>
2021-10-10 14:03:27 +1100

    created the NavTab functionality

---
+++ src/components/NavTabs.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+function NavTabs({ currentPage, handlePageChange }) {
+    return (
+        <ul>
+            <li>
+                <a
+                    href="#About"
+                    onClick={() => handlePageChange('About')}
+                    className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+                >
+                About
+                </a>
+            </li>
+            <li>
+                <a
+                    href="#Project"
+                    onClick={() => handlePageChange('Project')}
+                    className={currentPage === 'Project' ? 'nav-link active' : 'nav-link'}
+                >
+                Project
+                </a>
+            </li>
+            <li>
+                <a
+                    href="#Contact"
+                    onClick={() => handlePageChange('Contact')}
+                    className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                >
+                    Contact
+                </a>
+            </li>
+            <li>
+                <a
+                    href="#Resume"
+                >
+                    Resume
+                </a>
+            </li>
+        </ul>
+    )
+}
+
+export default NavTabs;
