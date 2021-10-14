a1433cb525d46cec85aa56fa91d7b97e358fb44c
Chandler Liang <chandler.liang123@gmail.com>
2021-10-14 16:12:16 +1100

    fixed the resume.js

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,24 +1,35 @@
 import React from "react";
 
 function Resume() {
+
+    const styles = {
+        margin: {
+            margin: "5px",
+        },
+        margin2x: {
+            margin: "50px"
+        },
+        a: {
+            textDecoration: "none",
+            color: "#7b9ea8"
+        }
+    }
+
     return (
-        <div>
+        <div class="container">
             <h2>
                 Resume
             </h2>
 
-            <p>Find my resume <a href="https://docs.google.com/document/d/1naeVIWekSseM93QtLfRcrRo-wRjQBMwUz6zJqmsnxLM/edit">here</a></p>
-            <div>
-                <h3>Skills</h3>
-                <ul>
-                    <i class="fab fa-html5"></i>
-                    <i class="fab fa-css3-alt"></i>
-                    <i class="fab fa-js"></i>
-                    <i class="fab fa-node"></i>
-                    <i class="fab fa-react"></i>
-                    <i class="fas fa-database"></i>
-                    <i class="fab fa-angular"></i>
-                </ul>
+            <h3>Find my resume <a style={styles.a} href="https://docs.google.com/document/d/1naeVIWekSseM93QtLfRcrRo-wRjQBMwUz6zJqmsnxLM/edit">here</a></h3>
+            <div class="d-flex justify-content-around" style={styles.margin2x}>
+                <i class="fab fa-html5 fa-5x" style={styles.margin}></i>
+                <i class="fab fa-css3-alt fa-5x" style={styles.margin}></i>
+                <i class="fab fa-js fa-5x" style={styles.margin}></i>
+                <i class="fab fa-node fa-5x" style={styles.margin}></i>
+                <i class="fab fa-react fa-5x" style={styles.margin}></i>
+                <i class="fas fa-database fa-5x" style={styles.margin}></i>
+                <i class="fab fa-angular fa-5x" style={styles.margin}></i>
             </div>
         </div>
     );
